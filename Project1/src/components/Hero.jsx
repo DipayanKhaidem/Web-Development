ea4ae69a246fe676c835502ca542829ecd3b3efe
Dipayan Khaidem <dipayansingha8@gmail.com>
2025-12-05 23:28:56 +0530

    Active tab

--- Project1/src/components/Hero.jsx
+++ Project1/src/components/Hero.jsx
@@ -2,6 +2,7 @@ import { ChevronDown } from "lucide-react";
 import {useEffect, useState} from "react";
 export default function Hero(){
     const [mousePosition,setMousePosition] =useState({x:0,y:0});
+    const [activeTab,setActiveTab]=useState("App.jsx");
 
     useEffect(()=>{
         function handleMouseMove(e){
@@ -42,9 +43,18 @@ export default function Hero(){
                     <div className="p-3 sm:p-4 relative-h-full">
                     {/* file tabs */}
                     <div className="flex space-x-1 sm:space-x-2 mb-3 sm:mb-4 overflow-x-auto">
-                        <button className="px-3 py-2 backdrop-blur-sm text-xs sm:text-sm rounded-t-lg border bg-white/5 text-gray-300 transition-all duration-200 whitespace-nowrap">App.jsx</button>
-                        <button className="px-3 py-2 backdrop-blur-sm text-xs sm:text-sm rounded-t-lg border bg-white/5 text-gray-300 transition-all duration-200 whitespace-nowrap">Hero.jsx</button>
-                        <button className="px-3 py-2 backdrop-blur-sm text-xs sm:text-sm rounded-t-lg border bg-white/5 text-gray-300 transition-all duration-200 whitespace-nowrap">Navbar.jsx</button>
+                        <button 
+                            onClick={()=>setActiveTab("App.jsx")}
+                        className={`px-3 py-2 backdrop-blur-sm text-xs sm:text-sm rounded-t-lg border 
+                            ${activeTab==="App.jsx"?"bg-blue-500 text-white border-blue-400/20":"bg-white/5 text-gray-300 border-white/10 hover:bg-white/10"} transition-all duration-200 whitespace-nowrap`}>App.jsx</button>
+                        <button 
+                          onClick={()=>setActiveTab("Hero.jsx")}
+                        className={`px-3 py-2 backdrop-blur-sm text-xs sm:text-sm rounded-t-lg border 
+                            ${activeTab==="Hero.jsx"?"bg-blue-500 text-white border-blue-400/20":"bg-white/5 text-gray-300 border-white/10 hover:bg-white/10"} transition-all duration-200 whitespace-nowrap`}>Hero.jsx</button>
+                        <button 
+                          onClick={()=>setActiveTab("Navbar.jsx")}
+                        className={`px-3 py-2 backdrop-blur-sm text-xs sm:text-sm rounded-t-lg border 
+                            ${activeTab==="Navbar.jsx"?"bg-blue-500 text-white border-blue-400/20":"bg-white/5 text-gray-300 border-white/10 hover:bg-white/10"} transition-all duration-200 whitespace-nowrap`}>Navbar.jsx</button>
                     </div>
                 </div>
                 </div>
